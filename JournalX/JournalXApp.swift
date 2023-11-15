5c356dcf9d59a16d40e274b46120bbebf9bedf33
KaranO-Dev <119845259+KaranO-Dev@users.noreply.github.com>
2023-11-15 13:04:16 +0100

    Initial commit

---
+++ JournalX/JournalXApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalXApp.swift
+//  JournalX
+//
+//  Created by Karan Oroumchi on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct JournalXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# JournalX
+ Recreating JournalX
